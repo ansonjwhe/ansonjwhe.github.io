a6b89451c5e7d36449e89d2d8c99a75eb4698ada
Anson He <ansonjwhe@gmail.com>
2021-02-16 02:11:25 -0500

    added card slider template

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './CardSection.css';
+import CardSlider from './CardSlider';
 
 function CardSection() {
     return (
         <div className='container'>
             <h1>Experiences</h1>
+            <CardSlider />
         </div>
     )
 }
---
+++ src/components/CardSlider.js
@@ -0,0 +1,40 @@
+// import Swiper core and required modules
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import './CardSlider.css'
+
+// import 'swiper-bundle.css'
+// Import Swiper styles
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+function CardSlider() {
+  return (
+    <div id='slider-container'>
+      <Swiper
+        loop = {true}
+        centeredSlides = {true}
+        slidesPerView = {3}
+        spaceBetween = {10}
+        navigation
+        pagination = {{ clickable: true }}
+        setWrapperSize = {true}
+        onSwiper = {(swiper) => console.log(swiper)}
+        onSlideChange = {() => console.log('slide change')}
+      >
+        <SwiperSlide class='swiper-slide'>Slide 1</SwiperSlide>
+        <SwiperSlide class='swiper-slide'>Slide 2</SwiperSlide>
+        <SwiperSlide class='swiper-slide'>Slide 3</SwiperSlide>
+        <SwiperSlide class='swiper-slide'>Slide 4</SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
+
+export default CardSlider;
