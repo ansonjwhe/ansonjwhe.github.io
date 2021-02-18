e621f03d60f92421bc35738373e6067058444eef
Anson He <ansonjwhe@gmail.com>
2021-02-18 01:49:17 -0500

    added fade-in animation to HeroSection

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar'
+import Fade from 'react-reveal'
+
 import '../../App.css';
 import './HeroSection.css';
 
 export default function HeroSection() {
   return (
+    // <Fade top>
     <div className='hero-container'>
       {/* <video src='/videos/video-1.mp4' autoPlay loop muted /> */}
-      
+      <Fade top>
       <Avatar alt='Profile Photo' id='avatar' src='/images/self.jpg'/>
+      </Fade>
+      <Fade left>
       <h1>Hey! I'm Anson He</h1>
+      </Fade>
+      <Fade right>
       <p>I'm an aspiring developer studying Computing and Financial Management at the University of Waterloo.</p>
-
+      </Fade>
+      <Fade bottom>
+      <div>
       <div class='social-icons'>
         <a
           class='social-icon-link email' 
@@ -50,6 +59,9 @@ export default function HeroSection() {
           <i class='fab fa-instagram' />
         </a>
       </div>
+      </div>
+      </Fade>
     </div>
+    // {/* </Fade> */}
   );
 }
