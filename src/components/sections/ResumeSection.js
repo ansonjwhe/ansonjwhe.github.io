ca0c618f2e3de549899ddc490dca5da589b78a80
Anson He <ansonjwhe@gmail.com>
2021-02-21 01:08:38 -0500

    Fixed manifest, Updated Resume embed

--- src/components/sections/ResumeSection.js
+++ src/components/sections/ResumeSection.js
@@ -7,13 +7,13 @@ export default function ResumeSection() {
   return (
     <div id='resume-container'>
       <Fade top>
-        <h1>My Current Resume</h1>
+        <h1>Current Resume</h1>
       </Fade>
       <Fade bottom>
       <iframe
         title='current resume'
-        src='https://drive.google.com/file/d/1Kj5OGoMfIXsr1FeRq8OkJgA4rbITFt_P/preview'
-        frameborder='0' 
+        src='https://drive.google.com/file/d/1NpB-Jdf0AAuXX3xALsdWIXcL6Yj6crKR/preview'
+        frameborder='0'
         allowfullscreen 
         allow='fullscreen'
       />
