cfbd6b29eba9e21c8c1404a5481e22b9a6e2481b
Anson He <ansonjwhe@gmail.com>
2021-09-05 21:36:27 -0400

    Updated current resume

--- src/components/sections/ResumeSection.js
+++ src/components/sections/ResumeSection.js
@@ -12,7 +12,7 @@ export default function ResumeSection() {
       <Fade bottom>
       <iframe
         title='current resume'
-        src='https://drive.google.com/file/d/1NpB-Jdf0AAuXX3xALsdWIXcL6Yj6crKR/preview'
+        src='https://drive.google.com/file/d/11OoZ1CN74k42DIfoWHyDjniNeWi2IulT/preview'
         frameborder='0'
         allowfullscreen 
         allow='fullscreen'
